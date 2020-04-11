6a9c4be6a1ce845bf73e7d9e3959156366ae9a47
taypal <tay.houk@yahoo.com>
2020-04-11 10:28:20 -0600

    added table to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Table from "./Components/employeeTable";
 
 function App() {
     return (
         <div>
             <header />
+            <Table />
         </div>
     )
 }
