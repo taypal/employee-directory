2b836cfdd02c15adbca1efbd3b0fbbd45174f953
taypal <tay.houk@yahoo.com>
2020-04-11 10:55:42 -0600

    fixed table

--- src/Components/employeeTable.js
+++ src/Components/employeeTable.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { Conponent, Component } from "react";
 import Employee from "./employee";
 import employeeList from "../employee.json";
+import employee from "./employee";
 
-function Table(props) {
-    return (
-        <table>
-            <tr>
-                <td>{props.id}</td>
-                <td>{props.first_name}</td>
-                <td>{props.email}</td>
-                <td>{props.role}</td>
-            </tr>
-            <Employee />
-        </table>
-    )
+class Table extends Component {
+
+    state = {
+        employeeList
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <td>id</td>
+                        <td>first_name</td>
+                        <td>email</td>
+                        <td>role</td>
+                    </tr>
+                    {this.state.employeeList.map(employee => (
+
+                        <Employee
+                            name={employee.first_name}
+                            email={employee.email}
+                            position={employee.role}
+                        />
+                    ))}
+                </table>
+            </div>
+        )
+    }
 }
 
 export default Table;
